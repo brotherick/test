aef2565035b6f44146116952491fe633ec2ed2dd
Erick Jeronimo <erick@titonsoft.com>
2018-09-06 14:04:01 -0600

    relation clients-invoice created

--- database/migrations/2018_09_06_195808_add_client_id_to_invoice.php
+++ database/migrations/2018_09_06_195808_add_client_id_to_invoice.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateInvoiceTable extends Migration
+class AddClientIdToInvoice extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,10 @@ class CreateInvoiceTable extends Migration
      */
     public function up()
     {
-        Schema::create('invoice', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('serie');
-            $table->string('monto');
-            $table->string('balance');
-            $table->timestamps();
-        });
+      Schema::table('invoice', function (Blueprint $table) {
+          $table->integer('client_id')->unsigned();
+          $table->foreign('client_id')->references('id')->on('clients');
+      });
     }
 
     /**
@@ -29,6 +26,6 @@ class CreateInvoiceTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('invoice');
+        //
     }
 }
