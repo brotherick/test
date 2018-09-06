32184d3581361f4682114d0209e409cb226bed42
Erick Jeronimo <erick@titonsoft.com>
2018-09-06 17:00:51 -0600

    added layouts

---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+  <!-- Optional theme -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+  <!-- Latest compiled and minified JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  <title></title>
+</head>
+<body>
+  <div class="jumbotron">
+    <h1> TEST </h1>
+  </div>
+  @yield('content');
+  <div class="col-md-2 col-md-offset-5">
+    <a href="/">
+    <button type="button" class="btn btn-lg btn-info"name="button">Home</button>
+    </a>
+  </div>
+</div>
+</body>
+</html>
--- resources/views/pend_balance_clients.blade.php
+++ resources/views/pend_balance_clients.blade.php
@@ -1,18 +1,5 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-<head>
-  <meta charset="utf-8">
-  <!-- Latest compiled and minified CSS -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-  <!-- Optional theme -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-  <!-- Latest compiled and minified JavaScript -->
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-  <title></title>
-</head>
-<body>
+@extends('layouts.base');
+@section('content')
   <div class="container">
     <table class="table table-striped">
       <thead>
@@ -32,11 +19,5 @@
         @endforeach
       </tbody>
     </table>
-    <div class="col-md-2 col-md-offset-5">
-      <a href="/">
-      <button type="button" class="btn btn-lg btn-info"name="button">Home</button>
-      </a>
-    </div>
   </div>
-</body>
-</html>
+@endsection
